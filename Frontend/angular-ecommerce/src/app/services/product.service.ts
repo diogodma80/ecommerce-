557815933a5f277f186ecea2e59f82efe74a3e95
Diogo Almeida <diogodma80@gmail.com>
2020-11-06 14:39:15 -0500

    Enhanced search for product by category  - Modify Spring Boot app - Expose entity ids by autowiring the JPA entity manager to the MyDataRestConfig class   -  generate class common/product-category  - create class ProductCategory  - Create new component menu   -  generate component components/product-category-menu  - Enhance menu component to read data from product service  - Update product service to call URL on Spring Boot app  - In HTML, replace hard-coded links with meny component

--- Frontend/angular-ecommerce/src/app/services/product.service.ts
+++ Frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,29 +3,47 @@ import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
   private baseUrl = 'http://localhost:8080/api/products';
+
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
+  // service method called from ProductListComponent
   getProductList(theCategoryId: number): Observable<Product[]> {
 
     // build url based on the category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
+
+  // service method called from ProductCategoryMenuComponent
+  getProductCategories(): Observable<ProductCategory[]> { 
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+    
+  }
 }
 
-interface GetResponse {
+// interfaces to unwrap the JSON from Spring Data REST _embedded entry
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
