fdc77d0135b351ab0387b2113a43975f3d11a3b9
Diogo Almeida <diogodma80@gmail.com>
2020-11-14 16:12:12 -0500

    Checkout form layout  - Add support for reactive form   - Import ReactiveFormsModule in app.module.ts  - Define form in component checkout.component.ts file

--- Frontend/angular-ecommerce/src/app/app.module.ts
+++ Frontend/angular-ecommerce/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 //defining the routes
 const routes: Routes = [
@@ -42,7 +43,8 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
